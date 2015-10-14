53f3adf90bda5b0de2327088a144943a117557b9
Adrian Parker <iamhiddensomewhere@gmail.com>
2015-10-13 21:56:34 -0400

    - wording

--- app/test_server.py
+++ app/test_server.py
@@ -8,16 +8,15 @@ def client():
 
 
 def test_hello_world(client):
-    rv = client.get('/')
-    print(rv.data)
-    assert b'Hello World!' == rv.data
+    response = client.get('/')
+    assert b'Hello World!' == response.data
 
 
 def test_404(client):
-    rv = client.get('/non-exist')
-    assert rv.status_code == 404
+    response = client.get('/non-exist')
+    assert response.status_code == 404
 
 
 def test_five_oh_oh(client):
-    rv = client.get('/five-oh-oh')
-    assert rv.status_code == 500
+    response = client.get('/five-oh-oh')
+    assert response.status_code == 500
