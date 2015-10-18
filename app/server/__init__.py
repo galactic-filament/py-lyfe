94c3d27ff90fd4d2f47d73ea53e71554a0602363
Adrian Parker <iamhiddensomewhere@gmail.com>
2015-10-17 20:29:03 -0400

    Perform additional json payload testing.

--- app/server/__init__.py
+++ app/server/__init__.py
@@ -1,4 +1,6 @@
 from flask import Flask, jsonify, request
+from app import arithmetic
+import functools
 
 app = Flask(__name__)
 
@@ -11,3 +13,10 @@ def hello_world():
 @app.route('/receive-json', methods=['POST'])
 def receive_json():
     return jsonify(request.get_json())
+
+
+@app.route('/add', methods=['POST'])
+def add():
+    payload = request.get_json()
+    sum = functools.reduce(arithmetic.add, payload['numbers'])
+    return jsonify({'sum': sum})
--- app/tests/test_server.py
+++ app/tests/test_server.py
@@ -1,6 +1,7 @@
 import pytest
 import json
-from app import server
+from app import server, arithmetic
+import functools
 
 
 @pytest.fixture
@@ -30,3 +31,17 @@ def test_receive_json(client):
     assert response.status_code == 200
     d = json.loads(response.get_data(as_text=True))
     assert d['title'] == payload['title']
+
+
+def test_add(client):
+    numbers = range(1, 10)
+    payload = {'numbers': list(numbers)}
+    response = client.post(
+        '/add',
+        data=json.dumps(payload),
+        content_type='application/json'
+    )
+
+    assert response.status_code == 200
+    d = json.loads(response.get_data(as_text=True))
+    assert d['sum'] == functools.reduce(arithmetic.add, numbers)
