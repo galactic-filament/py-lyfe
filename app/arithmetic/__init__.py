7ac5d374fc07040791ed2ca5b1359ab54b5aa574
Adrian Parker <iamhiddensomewhere@gmail.com>
2016-02-21 18:13:08 -0500

    Remove unused code.

--- app/arithmetic/__init__.py
+++
@@ -1,6 +0,0 @@
-def subtract(x, y):
-    return x - y
-
-
-def add(x, y):
-    return x + y
--- app/tests/test_arithmetic.py
+++
@@ -1,9 +0,0 @@
-from app import arithmetic
-
-
-def test_subtract():
-    assert arithmetic.subtract(4, 2) == 2
-
-
-def test_add():
-    assert arithmetic.add(2, 2) == 4
