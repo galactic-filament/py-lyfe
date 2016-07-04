e22100b86801c2b2f6ac1ee8716c14380ac999ab
Adrian Parker <adrian@debian>
2016-07-04 13:58:52 -0400

    Add request logging to file.

--- app/app/__init__.py
+++ app/app/__init__.py
@@ -1,10 +1,24 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 import os
+import logging
+from pythonjsonlogger import jsonlogger
 from app import default, posts
 
 # flask init
 app = Flask(__name__)
+
+# flask logging
+log_handler = logging.FileHandler('/var/log/app.log')
+log_handler.setFormatter(jsonlogger.JsonFormatter())
+app.logger.setLevel(logging.INFO)
+app.logger.addHandler(log_handler)
+@app.after_request
+def log_request(response):
+    app.logger.info('Url hit', extra={
+        'status': response.status_code
+    })
+    return response
 
 # db init
 uri = 'postgres://postgres@{0}/postgres'.format(os.environ['DATABASE_HOST'])
