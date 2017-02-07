01b21c8b61815ec84a8c3d07ef80084245f8fe04
Adrian Parker <adrian.parker@smith.co>
2017-02-07 10:54:15 -0500

    Remove conditional logging.
    Add log dir to gitignore.

--- app/app/__init__.py
+++ app/app/__init__.py
@@ -10,21 +10,20 @@ from app import default, posts
 app = Flask(__name__)
 
 # flask logging
-if os.environ.get('REQUEST_LOGGING'):
-    log_handler = logging.FileHandler('{0}/app.log'.format(os.environ['APP_LOG_DIR']))
-    log_handler.setFormatter(jsonlogger.JsonFormatter())
-    app.logger.setLevel(logging.INFO)
-    app.logger.addHandler(log_handler)
-    @app.before_request
-    def log_request():
-        body = '' if not request.json else json.dumps(request.json)
+log_handler = logging.FileHandler('{0}/app.log'.format(os.environ['APP_LOG_DIR']))
+log_handler.setFormatter(jsonlogger.JsonFormatter())
+app.logger.setLevel(logging.INFO)
+app.logger.addHandler(log_handler)
+@app.before_request
+def log_request():
+    body = '' if not request.json else json.dumps(request.json)
 
-        app.logger.info('Url hit', extra={
-            'contentType': request.headers['content-type'],
-            'method': request.method,
-            'url': request.path,
-            'body': body
-        })
+    app.logger.info('Url hit', extra={
+        'contentType': request.headers['content-type'],
+        'method': request.method,
+        'url': request.path,
+        'body': body
+    })
 
 # db init
 uri = 'postgres://postgres@{0}/postgres'.format(os.environ['DATABASE_HOST'])
