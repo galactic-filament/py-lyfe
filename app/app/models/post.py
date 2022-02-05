c74cac6b58d44d6d6d3afe2e8813a853e99a555f
Adrian Parker <ihsw.aparker.kilabuk@gmail.com>
2022-02-04 20:07:03 -0700

    Move files around to make more sense.

--- app/app/models/post.py
+++ app/app/models/post.py
@@ -1,4 +1,4 @@
-from app.models import db
+from models import db
 
 
 class Post(db.Model):
--- app/app/tests/test_posts_routes.py
+++ app/app/tests/test_posts_routes.py
@@ -3,7 +3,7 @@ from unittest.mock import patch
 
 from requests import codes
 
-from app.models.post import Post
+from models.post import Post
 
 mock_create_post_body = {"body": "Hello, world!"}
 mock_post_id = 1
