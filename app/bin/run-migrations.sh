ad842a143c79d56940c465c5c9512cba438b99b5
Adrian Parker <ihsw.aparker.kilabuk@gmail.com>
2022-02-24 08:42:15 -0700

    Fix typo.

--- app/bin/run-migrations.sh
+++ app/bin/run-migrations.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
 
 source .venv/bin/activate \
-  && alembix -x db=dev revision --autogenerate -m 'autogen' \
+  && alembic -x db=dev revision --autogenerate -m 'autogen' \
   && alembic -x db=dev upgrade head
